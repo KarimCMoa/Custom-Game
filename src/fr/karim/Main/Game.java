95204259db7875122e8792f514f8fb53540efefa
KarimTouwni <belfaiezkarim.contactgmail.com>
2019-02-12 12:56:35 +0100

    Update Game.java
    RAGE

--- src/fr/karim/Main/Game.java
+++ src/fr/karim/Main/Game.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Game {
 	
 	public static void main(String[] args) {
-		JFrame window = new JFrame("saluu c mon jeu");
+		JFrame window = new JFrame("RAGE");
 		window.add(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
