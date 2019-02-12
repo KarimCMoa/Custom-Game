80b9799e270beaa271248268b8b9d0c55d02c766
KarimTouwni <belfaiezkarim.contactgmail.com>
2019-02-12 12:54:07 +0100

    Update Game.java
    j'ai enlevé un u

--- src/fr/karim/Main/Game.java
+++ src/fr/karim/Main/Game.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Game {
 	
 	public static void main(String[] args) {
-		JFrame window = new JFrame("saluuu c mon jeu");
+		JFrame window = new JFrame("saluu c mon jeu");
 		window.add(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
